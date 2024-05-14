d366f9aed31ef55361a5301761e289d0f2804f54
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-05-15 01:32:33 +0330

    Create api to edit and delete ads

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -77,8 +77,92 @@ export async function POST(req) {
 
 export async function PATCH(req) {
   try {
-    
-  } catch (error) {
-    
+    await connectDB();
+
+    const {
+      _id,
+      title,
+      description,
+      location,
+      phone,
+      realState,
+      price,
+      constructionDate,
+      category,
+      amenities,
+      rules,
+    } = await req.json();
+
+    const session = await getServerSession(req);
+    if (!session) {
+      return NextResponse.json(
+        {
+          error: "لطفا وارد حساب کاربری خود شوید",
+        },
+        { status: 401 }
+      );
+    }
+
+    const user = await User.findOne({ email: session.user.email });
+    if (!user) {
+      return NextResponse.json(
+        { error: "حساب کاربری یافت نشد" },
+        { status: 404 }
+      );
+    }
+
+    if (
+      !_id ||
+      !title ||
+      !location ||
+      !description ||
+      !phone ||
+      !realState ||
+      !price ||
+      !constructionDate ||
+      !category
+    ) {
+      return NextResponse.json(
+        { error: "لطفا اطلاعات معتبر وارد کنید" },
+        { status: 400 }
+      );
+    }
+
+    const profile = await Profile.findOne({ _id });
+    if (!user._id.equals(profile.userId)) {
+      return NextResponse.json(
+        {
+          error: "دستری شما به این آگهی محدود شده است",
+        },
+        { status: 403 }
+      );
+    }
+
+    profile.title = title;
+    profile.description = description;
+    profile.location = location;
+    profile.phone = phone;
+    profile.realState = realState;
+    profile.price = price;
+    profile.constructionDate = constructionDate;
+    profile.amenities = amenities;
+    profile.rules = rules;
+    profile.category = category;
+    profile.save();
+
+    return NextResponse.json(
+      {
+        message: "آگهی با موفقیت ویرایش شد",
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { error: "مشکلی در سرور رخ داده است" },
+      { status: 500 }
+    );
   }
 }
